29474ade0adf884e7f3e203bdcdc19a8805e5b4f
Brian Jordan <bcjordan@gmail.com>
2011-03-11 11:13:53 -0500

    insert sort

--- test.scm
+++ test.scm
@@ -2,6 +2,21 @@
 (define sum (l1) (foldl + 0 l1))
 (define is3 (l1) (map ((curry =) 3) l1))
 (define double (l1) (map ((curry *) 2) l1))
+(define triple (l1) (map ((curry *) 3) l1))
+
+(define insert (el l1) 
+  (if (null? l1)
+    (cons el l1)
+    (if (< el (car l1))
+      (cons el l1)
+      (cons (car l1) (insert el (cdr l1)))
+    )
+  ))
+
+(define sort (l1) (foldl insert '() l1))
+  
+(insert 3 '(0 1 2 4 5))
+(sort '(5 3 2 8 9 12))
 
 (define revapp (l1 l2)
   (if (null? l1)
@@ -13,5 +28,6 @@
 (sum a)
 (is3 a)
 (double a)
+(triple a)
 (revapp a '())
 (revapp a '(4 5 6))
